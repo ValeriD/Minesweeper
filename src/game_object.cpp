ecb59bcba0cd8b7d4abe967efa3efbfedfcff180
ValeriD <valeri.dobrev10@gmail.com>
2021-06-07 01:25:42 +0300

    Created game object that will be inherited by every object that will be drawn

---
+++ src/game_object.cpp
@@ -0,0 +1,40 @@
+#include "./include/game_object.hpp"
+
+GameObject::GameObject(int x, int y, int width, int height,const std::string id)
+: x(x), y(y), width(width), height(height), textureId(id)
+{
+}
+
+GameObject::GameObject(const GameObject& other)
+:  GameObject(other.x, other.y, other.width, other.height, other.textureId)
+{
+}
+
+GameObject& GameObject::operator=(const GameObject& other){
+    if(this == &other){
+        return *this;
+    }
+    this->x = other.x;
+    this->y = other.y;
+    this->width = other.width;
+    this->height = other.height;
+    this->textureId = other.textureId;
+
+    return *this;
+}
+
+int GameObject::getX() const{
+    return this->x;
+}
+int GameObject::getY() const{
+    return this->y;
+}
+int GameObject::getWidth() const{
+    return this->width;
+}
+int GameObject::getHeight() const{
+    return this->height;
+}
+std::string GameObject::getTextureId() const{
+    return this->textureId;
+}
---
+++ src/include/game_object.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include<string>
+
+class GameObject{
+    int x;
+    int y;
+
+    int width;
+    int height;
+
+    std::string textureId;
+
+public:
+    GameObject(int x, int y, int width, int height,const std::string id);
+
+    GameObject(const GameObject& other);
+    GameObject& operator=(const GameObject& other);
+
+    //TODO create cell and board
+
+    /**
+     * Getters
+     */
+    int getX() const;
+    int getY() const;
+    int getWidth() const;
+    int getHeight() const;
+    std::string getTextureId() const;
+
+    virtual void draw() = 0;
+    virtual void update() = 0;
+    virtual void clean() = 0;
+
+};
