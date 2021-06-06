7c5c61b07be8c740cb72135a3112ed369fefcb85
ValeriD <valeri.dobrev10@gmail.com>
2021-06-06 22:37:22 +0300

    Created singleton texture manager

---
+++ src/include/texture_manager.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include<string>
+#include<map>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+
+/**
+ * Singleton Texture manager
+ * @param textures - map of textures with ids
+ */
+class TextureManager{
+    std::map<std::string, SDL_Texture*> textures;
+
+    /** 
+     * Private default constructor
+     */
+    TextureManager();
+public:
+    /**
+     * Destructor
+     */
+    ~TextureManager();
+
+    /**
+     * Method that returns static instace of the manager
+     */
+    static TextureManager* getInstance();
+
+
+    /**
+     * Deleted assign operator and copy constructor
+     */
+    TextureManager(const TextureManager& other) = delete;  
+    TextureManager& operator=(const TextureManager& other) = delete;
+
+
+    /**
+     * Method that loads texture from given path
+     * @param path - the path of the image
+     * @param id - the id on which to be saved
+     * @param renderer 
+     * 
+     * @throw invalid_argument if the id already exists or invalid path provided
+     * @throw runtime_error if the texture creation failed
+     */
+    bool load(const std::string path, std::string id, SDL_Renderer* renderer);
+
+
+    /** 
+     * Method that draws the texture with the given id to the given position on the screen
+     * @param id - the ID of the texture
+     * @param srcx - the x position on which the coping should start
+     * @param srcy - the y position on which the coping should start
+     * @param destx - the x position on the screen
+     * @param desty - the y position on the screen
+     * @param renderer - the renderer
+     * 
+     * @throw invalid argument when invalid id passed
+     * @throw runtime_error when rendering fails
+     */
+    void draw(std::string& id, int srcx, int srcy ,int dstx, int dsty, int width, int height,SDL_Renderer* renderer);
+
+
+};
---
+++ src/texture_manager.cpp
@@ -0,0 +1,70 @@
+#include "./include/texture_manager.hpp"
+
+TextureManager::TextureManager(){
+
+}
+
+TextureManager::~TextureManager(){
+    //Destrys each texture in the map
+    for(auto it = this->textures.begin(); it != this->textures.end(); ++it){
+        SDL_DestroyTexture(it->second);
+    }
+}
+
+TextureManager* TextureManager::getInstance(){
+    static TextureManager textureManager;
+    return &textureManager;
+}
+
+bool TextureManager::load(const std::string path, std::string id, SDL_Renderer* renderer){
+    
+    if(this->textures.count(id)!=0){
+        throw std::invalid_argument("Texture with the given ID already exists!");
+    }
+    
+    //Loads into serface the image from the path specified
+    SDL_Surface* surface = IMG_Load(path.c_str());
+
+    //Checks if the surface is created
+    if(!surface){
+        throw std::invalid_argument("Invalid path provided!");
+    }
+
+    //Creates the texture from the surface
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_FreeSurface(surface);
+
+    //Checks if the texture is successcfully created
+    if(!texture){
+        throw std::runtime_error("Error during texture creation!");
+    }
+
+    //Adds the created texture in the map
+    this->textures.insert(std::pair<std::string, SDL_Texture*>(id, texture));
+    return true;
+
+
+}
+
+void TextureManager::draw(std::string& id, int srcx, int srcy ,int dstx, int dsty, int width, int height,SDL_Renderer* renderer){
+    if(this->textures.count(id) == 0){
+        throw std::invalid_argument("The given ID does not exist");
+    }
+
+    SDL_Rect src, dst;
+
+    src.w = dst.w = width;
+    src.h = dst.h = height;
+
+    src.x = srcx;
+    src.y = srcy;
+
+    dst.x = dstx;
+    dst.y = dsty;
+
+    if(SDL_RenderCopy(renderer,this->textures[id],&src,&dst)!=0){
+        throw std::runtime_error("Error when rendering!");
+    }
+
+}
